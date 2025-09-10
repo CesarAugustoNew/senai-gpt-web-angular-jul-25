758564229a7f44cb08e9548db778aa86b3dd329a
Cesar Augusto <cesarprofissional22@gmail.com>
2025-09-09 21:51:37 -0300

    Construindo novas requisicoes

---
+++ src/app/user-module/new-user-screen/new-user-screen.component.ts
@@ -0,0 +1,86 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-user-screen',
+  imports: [ReactiveFormsModule],
+  templateUrl: './new-user-screen.component.html',
+  styleUrl: './new-user-screen.component.css'
+})
+export class NewUserScreenComponent {
+  NewUserScreenComponent: FormGroup;
+
+  emailErrorMessage: string;
+  passwordErrorMessage: string;
+  sucessoErrorMessage: string;
+  incorretoErrorMessage: string;
+
+
+
+
+  constructor(private fb: FormBuilder) {
+    this.log = this.fb.group({
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]]
+    });
+
+    this.emailErrorMessage = "";
+    this.passwordErrorMessage = "";
+    this.sucessoErrorMessage = "";
+    this.incorretoErrorMessage = "";
+
+  }
+
+  async onLoginClick() {
+    alert("Botao de login clicado.");
+
+    console.log("Email", this.loginForm.value.email);
+    console.log("Password", this.loginForm.value.password);
+
+    if (this.loginForm.value.email == "") {
+      this.emailErrorMessage = "O campo de e-mail e obrigatorio.";
+      this.passwordErrorMessage = "";
+      return;
+    }
+
+    if (this.loginForm.value.password == "") {
+      this.passwordErrorMessage = "O campo de senha e obrigatorio.";
+      this.emailErrorMessage = "";
+      return;
+    }
+    
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+      method: "POST", // Enviar
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      })
+    });
+    console.log("STATUS CODE", response.status);
+    
+    if (response.status >= 200 && response.status <= 299) {
+      this.sucessoErrorMessage = "Login realizado com sucesso";
+      this.incorretoErrorMessage = ""
+      let json = await response.json();
+
+      let meuToken = json.accessToken;
+      let userId = json.user.id;
+
+      localStorage.setItem("meuToken", meuToken)
+      localStorage.setItem("meuId", userId)
+
+      window.location.href = "chat"
+      
+    }else {
+      this.incorretoErrorMessage = "Login deu errado";
+      this.sucessoErrorMessage = ""
+    }
+
+
+
+  }
+}
+ 
