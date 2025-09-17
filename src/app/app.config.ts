8079da4a36447ba333881f6381561aa2a6bf2395
Cesar Augusto <cesarprofissional22@gmail.com>
2025-09-17 19:54:20 -0300

    interinterceptador de login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, Router } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptor, HttpInterceptorFn, httpResource, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const router = inject(Router); //Direciona para as telas.
+
+  return next (req).pipe(
+
+    catchError((err: HttpErrorResponse) => {
+
+      if (err.status == 401) {
+        // Token expirou
+        localStorage.clear(); // Limpa todos dos dados do localStorage
+        router.navigate(['/login']);  // Redireciona para o login.
+      }
+
+      return throwError(() => err);
+
+    })
+  );
+
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideHttpClient(withInterceptors([ AuthInterceptor ])),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
